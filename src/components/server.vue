9926e50807c034687a016e9834db1490730cffce
Moaratorium <moaratorium@gmail.com>
2024-11-15 16:27:10 -0600

    added server/servers as duped landing, savepoint

---
+++ src/components/server.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { createRouter } from 'vue-router'
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+</script>
+
+<template>
+<h1>{{ msg }}</h1>
+  <div class="card">
+    <button type="button" @click="discordLogin">Log In With Discord</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,8 +1,12 @@
 import { createMemoryHistory, createRouter} from 'vue-router'
 import Landing from './components/landing.vue'
+import Servers from './components/servers.vue'
+import Server from './components/server.vue'
 
 const routes = [
-    {path: '/', component: Landing}
+    {path: '/', component: Landing},
+    {path: '/servers', component: Servers},
+    {path: '/server', component: Server}
 ]
 
 const router = createRouter({
