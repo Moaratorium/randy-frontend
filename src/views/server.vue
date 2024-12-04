79fc4c01a84daa78cabf7f78b1e6f03f07e8fc7e
Moaratorium <moaratorium@gmail.com>
2024-12-03 23:26:17 -0600

    simplified conditional rendering so it updates with initial fetch

--- src/views/server.vue
+++ src/views/server.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {mockServer} from '../../mocks.js'
+import { mockServer } from '../../mocks.js'
 import { ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 
@@ -9,37 +9,31 @@ const serverName = mockServer.name;
 const route = useRoute()
 
 const api = import.meta.env.VITE_BACKEND_API;
-const loading = ref(false)
-let queue = ref(null)
-const error = ref(null)
+
+const queue = ref(null)
 
 watch(() => route.params.id, getQueue, { immediate: true })
 
 async function getQueue() {
-  error.value = queue = null
-  loading.value = true
   try {
     const response = await fetch(`${api}/queue?guildId=${serverId}`);
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`)
       // custom error handling WIP
     }
-    queue = await response.json();
+    queue.value = await response.json();
     console.log(queue)
   } catch (error) {
     console.error(error.message)
-  } finally {
-    loading.value = false
-  }
+  } 
 }
 
 </script>
 
 <template>
 <h1>test:  The queue for server : {{ serverName + serverId }} </h1>
-<div v-if="loading" class="loading">Loading...</div>
-<div v-if="error" class="error">error test</div>
 <div v-if="queue" class="content">{{ queue }}</div>
+<div v-else class="content">Nothing is playing...</div>
 <button @click="getQueue"></button>
 </template>
 
--- src/views/servers.vue
+++ src/views/servers.vue
@@ -2,61 +2,46 @@
 import { ref, watch } from 'vue'
 import ServerTag from '../components/servertag.vue'
 import { useRoute } from 'vue-router'
-  
-const mockLiveData = ref([
-    {
-        "name": "Light Heaven/Lowrollers",
-        "id": "881111533332684298",
-        "icon": 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Basic_human_drawing.png/170px-Basic_human_drawing.png' // live returns null?
-    },
-    {
-        "name": "Moaratorium's Testland",
-        "id": "1207461053911122233",
-        "icon": 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Basic_human_drawing.png/170px-Basic_human_drawing.png'
-    }
-]);
 
 const route = useRoute()
 
 const api = import.meta.env.VITE_BACKEND_API;
-const loading = ref(false)
-let serverList = ref(null)
-const error = ref(null)
+
+const serverList = ref(null)
+
 
 watch(() => route.params.id, getServers, { immediate: true })
 
 async function getServers() {
-  error.value = serverList = null
-  loading.value = true
   try {
     const response = await fetch(`${api}/servers`);
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`)
       // custom error handling WIP
     }
-    serverList = await response.json();
+    serverList.value = await response.json();
     console.log(serverList)
+    forceRender()
   } catch (error) {
     console.error(error.message)
-  } finally {
-    loading.value = false
-  }
+  } 
 }
 
 </script>
 
 <template>
 <h1>Server List</h1>
-<div v-if="loading" class="loading">Loading...</div>
-<div v-if="error" class="error">error test</div>
 <div v-if="serverList" class="content">
   <ServerTag
   v-for="(server, index) in serverList"
-  :key="index"
+  :key="index + refreshCount"
   :id="server.id"
   :name="server.name"
   :image="server.icon"
   ></ServerTag>
+</div>
+<div v-else class="empty">
+  test: no servers
 </div>
 </template>
 
