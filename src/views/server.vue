ab99a5570b11370bb6fa365a4bef8188def3b3ad
Moaratorium <moaratorium@gmail.com>
2025-01-09 19:45:56 -0600

    cleaned comments, subbed mocks with live data

--- src/views/server.vue
+++ src/views/server.vue
@@ -1,43 +1,51 @@
 <script setup>
-import { mockServer } from '../../mocks.js'
+import SongTag from '../components/songtag.vue'
 import { ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 
-
-//remember the json comes in as array, meaning we maaay need to account for it by targeting an index or modifying response
-const serverName = ref(mockServer.name); //^^^
 const route = useRoute()
+const serverId = route.params.key;
+const serverName = ref(null);
 
 const api = import.meta.env.VITE_BACKEND_API;
 
-const queue = ref(mockServer.queue)
-const data = ref(null)
+const queue = ref(null)
+let data = '';
 
 watch(() => route.params.id, getQueue, { immediate: true })
 
+
 async function getQueue() {
   try {
+    if (!serverId) {
+      throw new Error(`Not in a valid server`)
+    }
     const response = await fetch(`${api}/server?guildId=${serverId}`);
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`)
     }
-    data.value = await response.json();
-    // parse json into queue and server info here, serverName
-    
-    //end
-    console.log(data)
-  } catch (error) {
+    data = await response.json();
+    queue.value = data[0].queue;
+    serverName.value = data[0].name; 
+    } catch (error) {
     console.error(error.message)
   } 
 }
-
 </script>
 
 <template>
 <h1>Playing for : {{ serverName }} </h1>
-<div v-if="queue" class="content">{{ queue }}</div>
+<div v-if="queue && queue.length !== 0" class="content">
+  <SongTag
+  v-for="(song, index) in queue"
+  :key="index"
+  :title="song.info.title"
+  :artist="song.info.author"
+  class="flex-child"
+  ></SongTag>
+</div>
 <div v-else class="content">Nothing is playing.</div>
-<button @click="getQueue"></button>
+<button @click="getQueue" class="refresh-button">refresh</button>
 </template>
 
 <style scoped>
