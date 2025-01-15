60b29b993f6d19458a264fb80f1760bb8168e936
Moaratorium <moaratorium@gmail.com>
2025-01-15 17:04:13 -0600

    built out queue buttons w/patches, stubbed removeLastSong

--- src/views/server.vue
+++ src/views/server.vue
@@ -31,6 +31,53 @@ async function getQueue() {
     console.error(error.message)
   } 
 }
+
+async function sendSkip() {
+  try {
+    if (!serverId) {
+      throw new Error(`Not in a valid server`)
+    }
+    const response = await fetch(`${api}/skipSong`,{
+      method: "PATCH",
+      body: JSON.stringify({ guildId: `${serverId}`}),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+    );
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`)
+    }
+    } catch (error) {
+    console.error(error.message)
+  } 
+}
+
+async function clearQueue(){
+  try {
+    if (!serverId) {
+      throw new Error(`Not in a valid server`)
+    }
+    const response = await fetch(`${api}/clearQueue`,{
+      method: "PATCH",
+      body: JSON.stringify({ guildId: `${serverId}`}),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+    );
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`)
+    }
+    } catch (error) {
+    console.error(error.message)
+  }
+}
+
+async function removeLastSong(){
+  console.log("This is a stub for later...")
+}
+
 </script>
 
 <template>
@@ -50,7 +97,11 @@ async function getQueue() {
   ></SongTag>
 </div>
 <div v-else class="content">Nothing is playing.</div>
-<button @click="getQueue" class="refresh-button">refresh</button>
+<div id="button-wrapper">
+  <button @click="sendSkip" class="server-button" id="skip-button">Skip</button>
+  <button @click="clearQueue" class="server-button" id="clear-button">Clear Queue</button>
+  <button @click="removeLastSong" class="server-button" id="remove-button">Remove Last</button>
+  <button @click="getQueue" class="server-button" id="refresh-button">Refresh</button></div>
 </template>
 
 <style scoped>
