4a8c13496c014dbe7941cf1b88bf9cb454d37543
Moaratorium <moaratorium@gmail.com>
2025-01-17 17:20:19 -0600

    built out commandTag component, builds into commands.vue from endpoint

--- src/views/commands.vue
+++ src/views/commands.vue
@@ -1,11 +1,46 @@
 <script setup>
+import { ref, onMounted } from 'vue'
+import CommandTag from '../components/commandtag.vue';
+
+const commandsList = ref(null);
+const api = import.meta.env.VITE_BACKEND_API;
+let data = '';
+
+onMounted(()=> {
+    getCommands();
+})
+
+async function getCommands() {
+  try {
+    const response = await fetch(`${api}/commands`);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`)
+    }
+    data = await response.json();
+    commandsList.value = data; 
+    console.log(data)
+    } catch (error) {
+    console.error(error.message)
+  } 
+}
 
 </script>
 
 <template>
-<div>
-    <p>This is where I would put commands, IF I HAD THEM</p>
+<div id="command-disclaimer">All commands can be used with either "/" or the server's specific prefix.</div>
+<div id="column-wrapper">
+      <div id="name-col" class="column">Command</div>
+      <div id="description-col" class="column">Description</div>
+    </div>
+<div v-if="commandsList && commandsList.length !== 0" class="content">
+  <CommandTag
+  v-for="(command, index) in commandsList"
+  :key="index"
+  :commandName="command.command"
+  :description="command.description"
+  ></CommandTag>
 </div>
+<div v-else class="content">Error: No Commands Listed.</div>
 </template>
 
 <style scoped>
